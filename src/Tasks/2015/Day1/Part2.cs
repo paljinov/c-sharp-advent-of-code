9c4bedfc54fd6ae02a31e9bd221ff3cf4b7fe093
Pave Aljinović <paljinovic@gmail.com>
2020-09-25 23:19:17 +0200

    Add "Day 1: Not Quite Lisp" solution

---
+++ src/Tasks/2015/Day1/Part2.cs
@@ -0,0 +1,50 @@
+﻿/*
+--- Part Two ---
+
+Now, given the same instructions, find the position of the first character that
+causes him to enter the basement (floor -1). The first character in the
+instructions has position 1, the second character has position 2, and so on.
+
+For example:
+
+- ) causes him to enter the basement at character position 1.
+- ()()) causes him to enter the basement at character position 5.
+
+What is the position of the character that causes Santa to first enter the
+basement?
+*/
+
+using App.Helpers;
+
+namespace App.Tasks.Year2015.Day1
+{
+    class Part2
+    {
+        public int Program()
+        {
+            int floor = 0;
+            string input = ReadInputHelper.ReadTaskInput(2015, 1);
+
+            for (int i = 0; i <= input.Length; i++)
+            {
+                char parenthesis = input[i];
+
+                if (parenthesis == '(')
+                {
+                    floor += 1;
+                }
+                else
+                {
+                    floor -= 1;
+                }
+
+                if (floor == -1)
+                {
+                    return i + 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
