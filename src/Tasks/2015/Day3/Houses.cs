6bc4d6f3f361fe46135535edb641e7ccb886a1c4
Pave Aljinović <paljinovic@gmail.com>
2020-09-29 20:17:21 +0200

    Add "Day 3: Perfectly Spherical Houses in a Vacuum" solution

---
+++ src/Tasks/2015/Day3/Houses.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+
+namespace App.Tasks.Year2015.Day3
+{
+    class Houses
+    {
+        public static HashSet<(int, int)> GetVisitedHousesLocationsForMoves(string moves)
+        {
+            HashSet<(int, int)> houseLocations = new HashSet<(int, int)>();
+
+            // Starting location is (0,0) we track visited houses by using Cartesian coordinate system
+            int x = 0;
+            int y = 0;
+
+            houseLocations.Add((x, y));
+
+            foreach (char move in moves)
+            {
+                switch (move)
+                {
+                    case '^': // north
+                        y += 1;
+                        break;
+                    case 'v': // south
+                        y -= 1;
+                        break;
+                    case '>': // east
+                        x += 1;
+                        break;
+                    case '<': // west
+                        x -= 1;
+                        break;
+                    default:
+                        break;
+                }
+
+                houseLocations.Add((x, y));
+            }
+
+            return houseLocations;
+        }
+    }
+}
---
+++ src/Tasks/2015/Day3/Part2.cs
@@ -0,0 +1,59 @@
+﻿/*
+--- Part Two ---
+
+The next year, to speed up the process, Santa creates a robot version of
+himself, Robo-Santa, to deliver presents with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents to the
+same starting house), then take turns moving based on instructions from the elf,
+who is eggnoggedly reading from the same script as the previous year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+- ^v delivers presents to 3 houses, because Santa goes north, and then
+  Robo-Santa goes south.
+- ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back
+  where they started.
+- ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction
+  and Robo-Santa going the other.
+*/
+
+using App.Helpers;
+using System.Text;
+
+namespace App.Tasks.Year2015.Day3
+{
+    class Part2 : ITask<int>
+    {
+        public int Program()
+        {
+            string moves = ReadInputHelper.ReadTaskInput(2015, 3);
+
+            StringBuilder santaMoves = new StringBuilder();
+            StringBuilder roboSantaMoves = new StringBuilder();
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                char move = moves[i];
+
+                if (i % 2 == 0)
+                {
+                    santaMoves.Append(move);
+                }
+                else
+                {
+                    roboSantaMoves.Append(move);
+                }
+            }
+
+            var santaHouseLocations = Houses.GetVisitedHousesLocationsForMoves(santaMoves.ToString());
+            var roboSantaHouseLocations = Houses.GetVisitedHousesLocationsForMoves(roboSantaMoves.ToString());
+
+            santaHouseLocations.UnionWith(roboSantaHouseLocations);
+
+            return santaHouseLocations.Count;
+        }
+    }
+}
