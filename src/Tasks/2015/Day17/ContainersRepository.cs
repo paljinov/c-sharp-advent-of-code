1db207687c2740714617cb2a3d18e04426ae11d2
Pave Aljinović <paljinovic@gmail.com>
2020-10-21 23:36:43 +0200

    Add "Day 17: No Such Thing as Too Much" parser

---
+++ src/Tasks/2015/Day17/ContainersRepository.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace App.Tasks.Year2015.Day17
+{
+    class ContainersRepository
+    {
+        public int[] ParseInput(string input)
+        {
+            string[] containersString = input.Split(Environment.NewLine);
+
+            int[] containers = new int[containersString.Length];
+            for (int i = 0; i < containersString.Length; i++)
+            {
+                containers[i] = int.Parse(containersString[i]);
+            }
+
+            return containers;
+        }
+    }
+}
---
+++ src/Tasks/2015/Day17/Part1.cs
@@ -0,0 +1,43 @@
+﻿/*
+--- Day 17: No Such Thing as Too Much ---
+
+The elves bought too much eggnog again - 150 liters this time. To fit it all
+into your refrigerator, you'll need to move it into smaller containers. You take
+an inventory of the capacities of the available containers.
+
+For example, suppose you have containers of size 20, 15, 10, 5, and 5 liters. If
+you need to store 25 liters, there are four ways to do it:
+
+- 15 and 10
+- 20 and 5 (the first 5)
+- 20 and 5 (the second 5)
+- 15, 5, and 5
+
+Filling all containers entirely, how many different combinations of containers
+can exactly fit all 150 liters of eggnog?
+*/
+
+namespace App.Tasks.Year2015.Day17
+{
+    class Part1 : ITask<int>
+    {
+        private readonly ContainersRepository containersRepository;
+
+        private readonly ContainersCombinations containersCombinations;
+
+        public Part1()
+        {
+            containersRepository = new ContainersRepository();
+            containersCombinations = new ContainersCombinations();
+        }
+
+        public int Solution(string input)
+        {
+            int[] containers = containersRepository.ParseInput(input);
+
+            // int combinations = containersCombinations.CalculateContainersCombinations(containers);
+
+            return 0;
+        }
+    }
+}
