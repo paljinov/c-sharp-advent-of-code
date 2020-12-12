844cba6d2f8c544f1dd00f637d2935cb2e794393
Pave Aljinović <paljinovic@gmail.com>
2020-12-12 09:03:44 +0100

    Add "Day 12: Rain Risk" tests

---
+++ tests/Tasks/2020/Day12/Part1Tests.cs
@@ -0,0 +1,28 @@
+using System;
+using App.Tasks.Year2020.Day12;
+using Xunit;
+
+namespace Tests.Tasks.Year2020.Day12
+{
+    public class Part1Tests
+    {
+        private readonly Part1 task;
+
+        public Part1Tests()
+        {
+            task = new Part1();
+        }
+
+        [Fact]
+        public void Solution_ShipNavigationActionsExample_ManhattanDistanceBetweenStartAndEndPositionEquals()
+        {
+            string shipNavigationActions = "F10"
+                + $"{Environment.NewLine}N3"
+                + $"{Environment.NewLine}F7"
+                + $"{Environment.NewLine}R90"
+                + $"{Environment.NewLine}F11";
+
+            Assert.Equal(25, task.Solution(shipNavigationActions));
+        }
+    }
+}
---
+++ tests/Tasks/2020/Day12/Part2Tests.cs
@@ -0,0 +1,28 @@
+using System;
+using App.Tasks.Year2020.Day12;
+using Xunit;
+
+namespace Tests.Tasks.Year2020.Day12
+{
+    public class Part2Tests
+    {
+        private readonly Part2 task;
+
+        public Part2Tests()
+        {
+            task = new Part2();
+        }
+
+        [Fact]
+        public void Solution_ShipNavigationActionsExample_ManhattanDistanceBetweenStartAndEndPositionByMovingWaypointEquals()
+        {
+            string shipNavigationActions = "F10"
+                + $"{Environment.NewLine}N3"
+                + $"{Environment.NewLine}F7"
+                + $"{Environment.NewLine}R90"
+                + $"{Environment.NewLine}F11";
+
+            Assert.Equal(286, task.Solution(shipNavigationActions));
+        }
+    }
+}
