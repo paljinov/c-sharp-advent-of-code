508c0c2485ab25c5dca1a43c68fc4cee9e17e637
Pave Aljinović <paljinovic@gmail.com>
2020-12-19 12:27:54 +0100

    Add "Day 18: Operation Order" part2 solution

--- src/Tasks/2020/Day18/ExpressionsEvaluator.cs
+++ src/Tasks/2020/Day18/ExpressionsEvaluator.cs
@@ -1,4 +1,4 @@
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace App.Tasks.Year2020.Day18
 {
@@ -16,76 +16,107 @@ namespace App.Tasks.Year2020.Day18
             return totalResult;
         }
 
+        public long ResultingValuesSumForAdditionBeforeMultiplication(string[] expressions)
+        {
+            long totalResult = 0;
+
+            foreach (string expression in expressions)
+            {
+                totalResult += CalculateResultForAdditionBeforeMultiplication(expression);
+            }
+
+            return totalResult;
+        }
+
         private long CalculateResultForSamePrecedence(string expression)
         {
             long result = 0;
 
-            for (int i = 0; i < expression.Length; i++)
+            Regex nestedBracketsRegex = new Regex(@"\([\d+*\s]+?\)");
+            Regex splitByOperationRegex = new Regex(@"(\s[\+\*]\s)");
+
+            MatchCollection nestedBracketsMatches = nestedBracketsRegex.Matches(expression);
+            // If there are nested brackets results in them are calculated first
+            if (nestedBracketsMatches.Count > 0)
             {
-                if (char.IsDigit(expression[i]))
+                foreach (Match nestedBracketMatch in nestedBracketsMatches)
                 {
-                    int integer = (int)char.GetNumericValue(expression[i]);
-                    result = CalculateNewResultForSamePrecedence(result, integer, expression, i);
+                    string nestedBracket = nestedBracketMatch.Groups[0].Value;
+                    long nestedBracketResult = CalculateResultForSamePrecedence(nestedBracket[1..^1]);
+                    expression = expression.Replace(nestedBracket, nestedBracketResult.ToString());
                 }
-                else if (expression[i] == '(')
+
+                return CalculateResultForSamePrecedence(expression);
+            }
+            // If there aren't any nested brackets
+            else
+            {
+                string[] splittedExpression = splitByOperationRegex.Split(expression);
+
+                for (int i = 0; i < splittedExpression.Length; i++)
                 {
-                    (string bracketsExpression, int endPosition) = GetExpressionsInMatchingBrackets(expression, i);
+                    string current = splittedExpression[i];
 
-                    long bracketsResult = CalculateResultForSamePrecedence(bracketsExpression);
-                    result = CalculateNewResultForSamePrecedence(result, bracketsResult, expression, i);
-
-                    i = endPosition + 1;
+                    bool isNumeric = int.TryParse(splittedExpression[i], out int integer);
+                    if (isNumeric)
+                    {
+                        // If first number or operation is addition
+                        if (i == 0 || splittedExpression[i - 1] == " + ")
+                        {
+                            result += integer;
+                        }
+                        // If operation is multiplication
+                        else
+                        {
+                            result *= integer;
+                        }
+                    }
                 }
             }
 
             return result;
         }
 
-        private long CalculateNewResultForSamePrecedence(long result, long integer, string expression, int i)
+        private long CalculateResultForAdditionBeforeMultiplication(string expression)
         {
-            if (i - 2 < 0 || expression[i - 2] == '+')
+            long result = 1;
+
+            Regex nestedBracketsRegex = new Regex(@"\([\d+*\s]+?\)");
+            Regex additionRegex = new Regex(@"(\d+)\s\+\s(\d+)");
+
+            MatchCollection nestedBracketsMatches = nestedBracketsRegex.Matches(expression);
+            // If there are nested brackets results in them are calculated first
+            if (nestedBracketsMatches.Count > 0)
             {
-                result += integer;
+                foreach (Match nestedBracketMatch in nestedBracketsMatches)
+                {
+                    string nestedBracket = nestedBracketMatch.Groups[0].Value;
+                    long nestedBracketResult = CalculateResultForAdditionBeforeMultiplication(nestedBracket[1..^1]);
+                    expression = expression.Replace(nestedBracket, nestedBracketResult.ToString());
+                }
+
+                return CalculateResultForAdditionBeforeMultiplication(expression);
             }
-            else if (expression[i - 2] == '*')
+            // If there aren't any nested brackets
+            else
             {
-                result *= integer;
+                // If there are additions which needs to be done first
+                while (expression.Contains("+"))
+                {
+                    Match additionMatch = additionRegex.Match(expression);
+                    long sum = long.Parse(additionMatch.Groups[1].Value) + long.Parse(additionMatch.Groups[2].Value);
+                    expression = additionRegex.Replace(expression, sum.ToString(), 1);
+                }
+
+                string[] integers = expression.Split(" * ");
+                // Only multiplication operations are left
+                foreach (string integer in integers)
+                {
+                    result *= long.Parse(integer);
+                }
             }
 
             return result;
-        }
-
-        private (string bracketExpression, int endPosition) GetExpressionsInMatchingBrackets(string expression, int i)
-        {
-            int openedBrackets = 0;
-            int endPosition = 0;
-
-            StringBuilder bracketExpression = new StringBuilder();
-
-            for (int j = i; j < expression.Length; j++)
-            {
-                if (expression[j] == '(')
-                {
-                    openedBrackets++;
-                    if (openedBrackets == 1)
-                    {
-                        continue;
-                    }
-                }
-                else if (expression[j] == ')')
-                {
-                    openedBrackets--;
-                    if (openedBrackets == 0)
-                    {
-                        endPosition = j;
-                        break;
-                    }
-                }
-
-                bracketExpression.Append(expression[j]);
-            }
-
-            return (bracketExpression.ToString(), endPosition);
         }
     }
 }
