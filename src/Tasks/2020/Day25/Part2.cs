4712637cffa5ac6597532d0a99d1b0d3ff48a67c
Pave Aljinović <paljinovic@gmail.com>
2020-12-25 17:54:11 +0100

    Add "Day 25: Combo Breaker" part2 solution

---
+++ src/Tasks/2020/Day25/Part2.cs
@@ -0,0 +1,50 @@
+/*
+--- Part Two ---
+
+The light turns green and the door unlocks. As you collapse onto the bed in your
+room, your pager goes off!
+
+"It's an emergency!" the Elf calling you explains. "The soft serve machine in
+the cafeteria on sub-basement 7 just failed and you're the only one that knows
+how to fix it! We've already dispatched a reindeer to your location to pick you
+up."
+
+You hear the sound of hooves landing on your balcony.
+
+The reindeer carefully explores the contents of your room while you figure out
+how you're going to pay the 50 stars you owe the resort before you leave.
+Noticing that you look concerned, the reindeer wanders over to you; you see that
+it's carrying a small pouch.
+
+"Sorry for the trouble," a note in the pouch reads. Sitting at the bottom of the
+pouch is a gold coin with a little picture of a starfish on it.
+
+Looks like you only needed 49 stars after all.
+*/
+
+/*
+################################################################################
+Part 2 is automatically solved if all of the puzzles before are solved, it was
+just needed to click on the link to get part2 star.
+################################################################################
+*/
+
+/*
+You spend all fifty stars to cover the room deposit!
+
+As you fix the soft serve machine, Santa offers you a ride in his sleigh; maybe
+the resort has a chimney you can use...
+
+Congratulations! You've finished every puzzle in Advent of Code 2020! I hope you
+had as much fun solving them as I had making them for you. I'd love to hear
+about your adventure; you can get in touch with me via contact info on my
+website or through Twitter.
+
+If you'd like to see more things like this in the future, please consider
+supporting Advent of Code and sharing it with others.
+
+To hear about future projects, you can follow me on Twitter.
+
+I've highlighted the easter eggs in each puzzle, just in case you missed any.
+Hover your mouse over them, and the easter egg will appear.
+*/
