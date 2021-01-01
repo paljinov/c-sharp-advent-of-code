ea5bff55ef15b281e28c1b54cf28b210038e1677
Pave Aljinović <paljinovic@gmail.com>
2021-01-01 08:54:24 +0100

    Add "Day 23: Opening the Turing Lock" tests

---
+++ tests/Tasks/2015/Day23/Part1Tests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Reflection;
+using App.Tasks.Year2015.Day23;
+using Xunit;
+
+namespace Tests.Tasks.Year2015.Day23
+{
+    public class Part1Tests
+    {
+        private readonly Part1 task;
+
+        public Part1Tests()
+        {
+            task = new Part1();
+        }
+
+        [Fact]
+        public void Solution_InstructionsExample_RegisterAValueWhenTheProgramIsFinishedExecutingEquals()
+        {
+            string instructions = "inc a"
+                + $"{Environment.NewLine}jio a, +2"
+                + $"{Environment.NewLine}tpl a"
+                + $"{Environment.NewLine}inc a";
+
+            instructions = instructions.Replace('a', 'b');
+
+            Assert.Equal(2, task.Solution(instructions));
+        }
+    }
+}
