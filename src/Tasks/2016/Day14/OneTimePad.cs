ed80147208b7e5397ec3884a6ad6fb76e3542eab
Pave Aljinović <paljinovic@gmail.com>
2021-02-12 19:58:06 +0100

    Add "Day 14: One-Time Pad" part2 solution

--- src/Tasks/2016/Day14/OneTimePad.cs
+++ src/Tasks/2016/Day14/OneTimePad.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -9,32 +8,34 @@ namespace App.Tasks.Year2016.Day14
     {
         private const int INDEX_STARTS_AT = 0;
 
-        private const int KEY_INDEX = 64;
+        private const int SIXTY_FOURTH_KEY_INDEX = 64;
 
         private const int SEARCH_QUINTUPLET_HASHES = 1000;
 
-        public int GetIndexWhichProducesSixtyFourthKey(string salt)
+        private readonly Dictionary<string, string> hashCache = new Dictionary<string, string>();
+
+        public int GetIndexWhichProducesSixtyFourthKey(string salt, int additionalHashings = 0)
         {
-            Dictionary<int, string> keys = new Dictionary<int, string>();
+            int[] keysIndexes = new int[SIXTY_FOURTH_KEY_INDEX];
+            int nextIndex = 0;
 
             int i = INDEX_STARTS_AT;
-            while (keys.Count < KEY_INDEX)
+            while (nextIndex < SIXTY_FOURTH_KEY_INDEX)
             {
-                string hash = GetMd5HashForString($"{salt}{i}");
+                string hash = GenerateHash(salt, i, additionalHashings);
 
-                (bool isTriplet, char? repeatingCharacter) = CharacterRepeatsWantedNumberOfTimes(hash, 3);
+                (bool isTriplet, char? repeatingCharacter) = IsCharacterRepeating(hash, 3);
                 if (isTriplet)
                 {
                     for (int j = i + 1; j < i + 1 + SEARCH_QUINTUPLET_HASHES; j++)
                     {
-                        string potentialQuintupletHash = GetMd5HashForString($"{salt}{j}");
+                        hash = GenerateHash(salt, j, additionalHashings);
 
-                        (bool isQuintuplet, _) =
-                            CharacterRepeatsWantedNumberOfTimes(potentialQuintupletHash, 5, repeatingCharacter.Value);
-
+                        (bool isQuintuplet, _) = IsCharacterRepeating(hash, 5, repeatingCharacter.Value);
                         if (isQuintuplet)
                         {
-                            keys.Add(i, hash);
+                            keysIndexes[nextIndex] = i;
+                            nextIndex++;
                             break;
                         }
                     }
@@ -43,34 +44,61 @@ namespace App.Tasks.Year2016.Day14
                 i++;
             }
 
-            return keys.Keys.Last();
+            return keysIndexes[^1];
         }
 
-        private (bool, char?) CharacterRepeatsWantedNumberOfTimes(
-            string hash,
-            int repetitions,
-            char? repeatingCharacter = null
-        )
+        private string GenerateHash(string salt, int index, int additionalHashings)
+        {
+            string saltWithIndex = $"{salt}{index}";
+
+            // Fetching hash from cache
+            if (hashCache.ContainsKey(saltWithIndex))
+            {
+                return hashCache[saltWithIndex];
+            }
+
+            string hash = GetMd5HashForString($"{salt}{index}");
+            for (int i = 0; i < additionalHashings; i++)
+            {
+                hash = GetMd5HashForString(hash);
+            }
+
+            // Storing hash in cache
+            hashCache[saltWithIndex] = hash;
+
+            return hash;
+        }
+
+        /// <summary>
+        /// Check is character repeating in hash asked number of times.
+        /// </summary>
+        /// <param name="hash">Hash</param>
+        /// <param name="repetitions">Minimum number of repetitions</param>
+        /// <param name="repeatingCharacter">If null any character can repeat asked number of times,
+        /// otherwise exactly this character must repeat asked number of times</param>
+        /// <returns></returns>
+        private (bool, char?) IsCharacterRepeating(string hash, int repetitions, char? repeatingCharacter = null)
         {
             for (int i = 0; i <= hash.Length - repetitions; i++)
             {
                 char c = hash[i];
+                // If exact character must repeat
                 if (repeatingCharacter.HasValue)
                 {
                     c = repeatingCharacter.Value;
                 }
 
-                bool repeatsWantedNumberOfTimes = true;
+                bool repeatsAskedNumberOfTimes = true;
                 for (int j = 0; j < repetitions; j++)
                 {
                     if (c != hash[i + j])
                     {
-                        repeatsWantedNumberOfTimes = false;
+                        repeatsAskedNumberOfTimes = false;
                         break;
                     }
                 }
 
-                if (repeatsWantedNumberOfTimes)
+                if (repeatsAskedNumberOfTimes)
                 {
                     return (true, c);
                 }
