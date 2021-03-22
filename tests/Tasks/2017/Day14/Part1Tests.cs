ab271090f47075bd83b3495279589b1b4e8c5963
Pave Aljinović <paljinovic@gmail.com>
2021-03-22 20:06:49 +0100

    Add "Day 14: Disk Defragmentation" part1 tests

---
+++ tests/Tasks/2017/Day14/Part1Tests.cs
@@ -0,0 +1,23 @@
+using System;
+using App.Tasks.Year2017.Day14;
+using Xunit;
+
+namespace Tests.Tasks.Year2017.Day14
+{
+    public class Part1Tests
+    {
+        private readonly Part1 task;
+
+        public Part1Tests()
+        {
+            task = new Part1();
+        }
+
+        [Fact]
+        public void Solution_KeyExample_UsedSquaresEquals()
+        {
+            string key = "flqrgnkx";
+            Assert.Equal(8108, task.Solution(key));
+        }
+    }
+}
