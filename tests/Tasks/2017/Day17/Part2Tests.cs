7f3dd7e810313df04d24f32ab416e380c2a61e51
Pave Aljinović <paljinovic@gmail.com>
2021-03-30 17:55:16 +0200

    Add "Day 17: Spinlock" part2 tests

---
+++ tests/Tasks/2017/Day17/Part2Tests.cs
@@ -0,0 +1,22 @@
+using App.Tasks.Year2017.Day17;
+using Xunit;
+
+namespace Tests.Tasks.Year2017.Day17
+{
+    public class Part2Tests
+    {
+        private readonly Part2 task;
+
+        public Part2Tests()
+        {
+            task = new Part2();
+        }
+
+        [Fact]
+        public void Solution_StepsExample_ValueAfterZeroWhenFiftyMillionthValueIsInsertedEquals()
+        {
+            string danceMoves = "3";
+            Assert.Equal(1222153, task.Solution(danceMoves));
+        }
+    }
+}
