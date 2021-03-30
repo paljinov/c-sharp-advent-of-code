3df96efa61963ceb16aac02dd53d971ffe825912
Pave Aljinović <paljinovic@gmail.com>
2021-03-30 17:54:56 +0200

    Add "Day 17: Spinlock" part2 solution

---
+++ src/Tasks/2017/Day17/Part2.cs
@@ -0,0 +1,46 @@
+/*
+--- Part Two ---
+
+The spinlock does not short-circuit. Instead, it gets more angry. At least, you
+assume that's what happened; it's spinning significantly faster than it was a
+moment ago.
+
+You have good news and bad news.
+
+The good news is that you have improved calculations for how to stop the
+spinlock. They indicate that you actually need to identify the value after 0 in
+the current state of the circular buffer.
+
+The bad news is that while you were determining this, the spinlock has just
+finished inserting its fifty millionth value (50000000).
+
+What is the value after 0 the moment 50000000 is inserted?
+*/
+
+namespace App.Tasks.Year2017.Day17
+{
+    public class Part2 : ITask<int>
+    {
+        private const int TOTAL_REPETITIONS = 50000000;
+
+        private const int VALUE_AFTER = 0;
+
+        private readonly StepsRepository stepsRepository;
+
+        private readonly Spinlock spinlock;
+
+        public Part2()
+        {
+            stepsRepository = new StepsRepository();
+            spinlock = new Spinlock();
+        }
+
+        public int Solution(string input)
+        {
+            int steps = stepsRepository.GetNumberOfSteps(input);
+            int value = spinlock.CalculateValueAfterTotalRepetitions(steps, TOTAL_REPETITIONS, VALUE_AFTER);
+
+            return value;
+        }
+    }
+}
