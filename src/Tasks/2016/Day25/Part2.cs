fa11774208f879f5a62740a628d537e0c93dd34b
Pave Aljinović <paljinovic@gmail.com>
2021-04-10 13:13:28 +0200

    Add "Day 25: Clock Signal" part2 solution

---
+++ src/Tasks/2016/Day25/Part2.cs
@@ -0,0 +1,38 @@
+/*
+--- Part Two ---
+
+The antenna is ready. Now, all you need is the fifty stars required to generate
+the signal for the sleigh, but you don't have enough.
+
+You look toward the sky in desperation... suddenly noticing that a lone star has
+been installed at the top of the antenna! Only 49 more to go.
+*/
+
+/*
+################################################################################
+Part 2 is automatically solved if all of the puzzles before are solved, it was
+just needed to click on the link to get part2 star.
+################################################################################
+*/
+
+/*
+You activate all fifty stars and transmit the signal. The star atop the antenna
+begins to glow.
+
+Suddenly, you see the sleigh fly past you!
+
+Looks like Santa was already waiting for your signal.
+
+Congratulations! You've finished every puzzle in Advent of Code 2016! I hope you
+had as much fun solving them as I had making them for you. I'd love to hear
+about your adventure; you can get in touch with me via contact info on my
+website or through Twitter.
+
+If you'd like to see more things like this in the future, please consider
+supporting Advent of Code and sharing it with others.
+
+To hear about future projects, you can follow me on Twitter.
+
+I've highlighted the easter eggs in each puzzle, just in case you missed any.
+Hover your mouse over them, and the easter egg will appear.
+*/
