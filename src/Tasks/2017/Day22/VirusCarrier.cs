4a3ce1cf1fc533df7d3c6f05bb85eb80abcab52e
Pave Aljinović <paljinovic@gmail.com>
2021-04-20 19:17:54 +0200

    Add "Day 22: Sporifica Virus" part2 solution

--- src/Tasks/2017/Day22/VirusCarrier.cs
+++ src/Tasks/2017/Day22/VirusCarrier.cs
@@ -8,6 +8,10 @@ namespace App.Tasks.Year2017.Day22
 
         private const char CLEAN_NODE = '.';
 
+        private const char WEAKENED_NODE = 'W';
+
+        private const char FLAGGED_NODE = 'F';
+
         public int CountBurstsThatCausedAnInfection(Dictionary<(int, int), char> nodesMap, int totalBurstsOfActivity)
         {
             int burstsThatCausedAnInfection = 0;
@@ -67,6 +71,122 @@ namespace App.Tasks.Year2017.Day22
                         case Direction.RIGHT:
                             facingDirection = Direction.DOWN;
                             y--;
+                            break;
+                    }
+                }
+            }
+
+            return burstsThatCausedAnInfection;
+        }
+
+        public int CountBurstsThatCausedAnInfectionForEvolvedVirus(
+            Dictionary<(int, int), char> nodesMap,
+            int totalBurstsOfActivity
+        )
+        {
+            int burstsThatCausedAnInfection = 0;
+
+            Direction facingDirection = Direction.UP;
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < totalBurstsOfActivity; i++)
+            {
+                // If node is not infected
+                if (!nodesMap.ContainsKey((x, y)) || nodesMap[(x, y)] == CLEAN_NODE)
+                {
+                    nodesMap[(x, y)] = WEAKENED_NODE;
+
+                    switch (facingDirection)
+                    {
+                        case Direction.UP:
+                            facingDirection = Direction.LEFT;
+                            x--;
+                            break;
+                        case Direction.DOWN:
+                            facingDirection = Direction.RIGHT;
+                            x++;
+                            break;
+                        case Direction.LEFT:
+                            facingDirection = Direction.DOWN;
+                            y--;
+                            break;
+                        case Direction.RIGHT:
+                            facingDirection = Direction.UP;
+                            y++;
+                            break;
+                    }
+                }
+                // If node is weakened
+                else if (nodesMap[(x, y)] == WEAKENED_NODE)
+                {
+                    nodesMap[(x, y)] = INFECTED_NODE;
+
+                    switch (facingDirection)
+                    {
+                        case Direction.UP:
+                            y++;
+                            break;
+                        case Direction.DOWN:
+                            y--;
+                            break;
+                        case Direction.LEFT:
+                            x--;
+                            break;
+                        case Direction.RIGHT:
+                            x++;
+                            break;
+                    }
+
+                    burstsThatCausedAnInfection++;
+                }
+                // If node is infected
+                else if (nodesMap[(x, y)] == INFECTED_NODE)
+                {
+                    nodesMap[(x, y)] = FLAGGED_NODE;
+
+                    switch (facingDirection)
+                    {
+                        case Direction.UP:
+                            facingDirection = Direction.RIGHT;
+                            x++;
+                            break;
+                        case Direction.DOWN:
+                            facingDirection = Direction.LEFT;
+                            x--;
+                            break;
+                        case Direction.LEFT:
+                            facingDirection = Direction.UP;
+                            y++;
+                            break;
+                        case Direction.RIGHT:
+                            facingDirection = Direction.DOWN;
+                            y--;
+                            break;
+                    }
+                }
+                // If node is flagged
+                else
+                {
+                    nodesMap[(x, y)] = CLEAN_NODE;
+
+                    switch (facingDirection)
+                    {
+                        case Direction.UP:
+                            facingDirection = Direction.DOWN;
+                            y--;
+                            break;
+                        case Direction.DOWN:
+                            facingDirection = Direction.UP;
+                            y++;
+                            break;
+                        case Direction.LEFT:
+                            facingDirection = Direction.RIGHT;
+                            x++;
+                            break;
+                        case Direction.RIGHT:
+                            facingDirection = Direction.LEFT;
+                            x--;
                             break;
                     }
                 }
