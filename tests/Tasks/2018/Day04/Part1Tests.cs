32aa41fb10a642d532301e39591a7edd522df627
Pave Aljinović <paljinovic@gmail.com>
2021-04-27 17:22:24 +0200

    Add "Day 4: Repose Record" part1 tests

---
+++ tests/Tasks/2018/Day04/Part1Tests.cs
@@ -0,0 +1,40 @@
+using System;
+using App.Tasks.Year2018.Day4;
+using Xunit;
+
+namespace Tests.Tasks.Year2018.Day4
+{
+    public class Part1Tests
+    {
+        private readonly Part1 task;
+
+        public Part1Tests()
+        {
+            task = new Part1();
+        }
+
+        [Fact]
+        public void Solution_GuardsRecordsExample_ProductOfIdAndMinuteForGuardWhoIsMostLikelyToBeAsleepAtSpecificMinuteEquals()
+        {
+            string guardsRecords = "[1518-11-01 00:00] Guard #10 begins shift"
+                + $"{Environment.NewLine}[1518-11-01 00:05] falls asleep"
+                + $"{Environment.NewLine}[1518-11-01 00:25] wakes up"
+                + $"{Environment.NewLine}[1518-11-01 00:30] falls asleep"
+                + $"{Environment.NewLine}[1518-11-01 00:55] wakes up"
+                + $"{Environment.NewLine}[1518-11-01 23:58] Guard #99 begins shift"
+                + $"{Environment.NewLine}[1518-11-02 00:40] falls asleep"
+                + $"{Environment.NewLine}[1518-11-02 00:50] wakes up"
+                + $"{Environment.NewLine}[1518-11-03 00:05] Guard #10 begins shift"
+                + $"{Environment.NewLine}[1518-11-03 00:24] falls asleep"
+                + $"{Environment.NewLine}[1518-11-03 00:29] wakes up"
+                + $"{Environment.NewLine}[1518-11-04 00:02] Guard #99 begins shift"
+                + $"{Environment.NewLine}[1518-11-04 00:36] falls asleep"
+                + $"{Environment.NewLine}[1518-11-04 00:46] wakes up"
+                + $"{Environment.NewLine}[1518-11-05 00:03] Guard #99 begins shift"
+                + $"{Environment.NewLine}[1518-11-05 00:45] falls asleep"
+                + $"{Environment.NewLine}[1518-11-05 00:55] wakes up";
+
+            Assert.Equal(240, task.Solution(guardsRecords));
+        }
+    }
+}
