51e7c5f0b87e6acb8bbb93de977892fd1eb6d76a
Pave Aljinović <paljinovic@gmail.com>
2021-05-09 11:49:20 +0200

    Add "Day 11: Chronal Charge" part1 solution

---
+++ src/Tasks/2018/Day11/FuelCells.cs
@@ -0,0 +1,79 @@
+namespace App.Tasks.Year2018.Day11
+{
+    public class FuelCells
+    {
+        private const int GRID_SQUARE_LENGTH = 300;
+
+        private const int ADD_TO_RACK_ID = 10;
+
+        private const int SUBTRACT_FROM_FUEL_CELL_POWER_LEVEL = 5;
+
+        public (int, int) FindTopLeftFuelCellCoordinateOfTheSquareWithLargestTotalPower(int gridSerialNumber)
+        {
+            int[,] grid = new int[GRID_SQUARE_LENGTH, GRID_SQUARE_LENGTH];
+            for (int i = 0; i < GRID_SQUARE_LENGTH; i++)
+            {
+                for (int j = 0; j < GRID_SQUARE_LENGTH; j++)
+                {
+                    grid[i, j] = CalculateFuelCellPowerLevel(i + 1, j + 1, gridSerialNumber);
+                }
+            }
+
+            (int x, int y) = FindTopLeftFuelCellCoordinateForThreeTimesThreeSquareWithLargestTotalPower(grid);
+
+            return (x, y);
+        }
+
+        private int CalculateFuelCellPowerLevel(int x, int y, int gridSerialNumber)
+        {
+            int rackId = x + ADD_TO_RACK_ID;
+            int fuelCellPowerLevel = rackId * y;
+            fuelCellPowerLevel += gridSerialNumber;
+            fuelCellPowerLevel *= rackId;
+
+            string fuelCellPowerLevelString = $"{fuelCellPowerLevel}";
+            int hundredsDigit = 0;
+            if (fuelCellPowerLevelString.Length >= 3)
+            {
+                hundredsDigit = (int)char.GetNumericValue(fuelCellPowerLevelString[^3]);
+            }
+
+            fuelCellPowerLevel = hundredsDigit - SUBTRACT_FROM_FUEL_CELL_POWER_LEVEL;
+
+            return fuelCellPowerLevel;
+        }
+
+        private (int x, int y) FindTopLeftFuelCellCoordinateForThreeTimesThreeSquareWithLargestTotalPower(int[,] grid)
+        {
+            int x = 0;
+            int y = 0;
+            int largestTotalPower = 0;
+
+            for (int i = 0; i < GRID_SQUARE_LENGTH - 3; i++)
+            {
+                for (int j = 0; j < GRID_SQUARE_LENGTH - 3; j++)
+                {
+                    int totalPower = 0;
+
+                    for (int k = i; k < i + 3; k++)
+                    {
+                        for (int h = j; h < j + 3; h++)
+                        {
+                            totalPower += grid[k, h];
+                        }
+                    }
+
+                    if (totalPower > largestTotalPower)
+                    {
+                        largestTotalPower = totalPower;
+                        // x and y start from 1
+                        x = i + 1;
+                        y = j + 1;
+                    }
+                }
+            }
+
+            return (x, y);
+        }
+    }
+}
---
+++ src/Tasks/2018/Day11/GridSerialNumberRepository.cs
@@ -0,0 +1,11 @@
+namespace App.Tasks.Year2018.Day11
+{
+    public class GridSerialNumberRepository
+    {
+        public int GetGridSerialNumber(string input)
+        {
+            int gridSerialNumber = int.Parse(input);
+            return gridSerialNumber;
+        }
+    }
+}
