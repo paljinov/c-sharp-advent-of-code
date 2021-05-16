6cdc146a9f36e3d3a11b9646c6ee00d4d0744685
Pave Aljinović <paljinovic@gmail.com>
2021-05-16 09:31:37 +0200

    Add "Day 1: The Tyranny of the Rocket Equation" part2 solution

--- src/Tasks/2019/Day01/FuelCounter.cs
+++ src/Tasks/2019/Day01/FuelCounter.cs
@@ -6,17 +6,35 @@ namespace App.Tasks.Year2019.Day1
 
         private const int SUBTRACT = 2;
 
-        public int CalculateSumOfTheFuelRequirementsForAllOfTheModules(int[] modulesMasses)
+        public int CalculateSumOfTheFuelRequirementsForAllOfTheModules(
+            int[] modulesMasses,
+            bool accountMassOfAddedFuel = false
+        )
         {
             int sumOfTheFuelRequirementsForAllOfTheModules = 0;
 
             foreach (int moduleMass in modulesMasses)
             {
-                int moduleFuelRequirements = (int)(moduleMass / DIVIDE_BY) - SUBTRACT;
+                int moduleFuelRequirements = CalculateModuleFuelRequirements(moduleMass, accountMassOfAddedFuel);
                 sumOfTheFuelRequirementsForAllOfTheModules += moduleFuelRequirements;
             }
 
             return sumOfTheFuelRequirementsForAllOfTheModules;
+        }
+
+        private int CalculateModuleFuelRequirements(int moduleMass, bool accountMassOfAddedFuel)
+        {
+            int moduleFuelRequirements = (int)(moduleMass / DIVIDE_BY) - SUBTRACT;
+            if (moduleFuelRequirements < 0)
+            {
+                moduleFuelRequirements = 0;
+            }
+            else if (accountMassOfAddedFuel)
+            {
+                moduleFuelRequirements += CalculateModuleFuelRequirements(moduleFuelRequirements, true);
+            }
+
+            return moduleFuelRequirements;
         }
     }
 }
