602564f8729023e97173475b7bfc2d595f75f675
Pave Aljinović <paljinovic@gmail.com>
2021-05-20 18:01:14 +0200

    Add "Day 3: Crossed Wires" part2 solution

--- src/Tasks/2019/Day03/CrossedWires.cs
+++ src/Tasks/2019/Day03/CrossedWires.cs
@@ -14,26 +14,53 @@ namespace App.Tasks.Year2019.Day3
             Dictionary<int, List<Instruction>> wiresPaths
         )
         {
-            Dictionary<int, List<(int, int)>> wiresLocations = new Dictionary<int, List<(int, int)>>();
-
-            foreach (KeyValuePair<int, List<Instruction>> wirePath in wiresPaths)
-            {
-                List<(int, int)> wireLocations = GetWireLocations(wirePath.Value);
-                wiresLocations.Add(wirePath.Key, wireLocations);
-            }
-
-            IEnumerable<(int, int)> wiresIntersections =
-                wiresLocations[FIRST_WIRE].Intersect(wiresLocations[SECOND_WIRE]);
-
+            List<(int, int)> wiresIntersections = CalculateWiresIntersections(wiresPaths);
             int minManhattanDistance = CalculateMinManhattanDistance(wiresIntersections);
 
             return minManhattanDistance;
         }
 
-        private List<(int, int)> GetWireLocations(List<Instruction> instructions)
+        public int CalculateFewestCombinedStepsTheWiresMustTakeToReachAnIntersection(
+            Dictionary<int, List<Instruction>> wiresPaths
+        )
         {
-            List<(int, int)> wireLocations = new List<(int, int)>();
+            Dictionary<int, Dictionary<int, (int, int)>> wiresLocationsWithSteps =
+                new Dictionary<int, Dictionary<int, (int, int)>>();
 
+            foreach (KeyValuePair<int, List<Instruction>> wirePath in wiresPaths)
+            {
+                Dictionary<int, (int, int)> wireLocationsWithSteps = GetWireLocationsWithSteps(wirePath.Value);
+                wiresLocationsWithSteps.Add(wirePath.Key, wireLocationsWithSteps);
+            }
+
+            List<(int, int)> wiresIntersections = CalculateWiresIntersections(wiresPaths);
+
+            int fewestCombinedSteps = CalculateFewestCombinedSteps(wiresLocationsWithSteps, wiresIntersections);
+
+            return fewestCombinedSteps;
+        }
+
+        private List<(int, int)> CalculateWiresIntersections(Dictionary<int, List<Instruction>> wiresPaths)
+        {
+            Dictionary<int, List<(int, int)>> wiresLocations = new Dictionary<int, List<(int, int)>>();
+
+            foreach (KeyValuePair<int, List<Instruction>> wirePath in wiresPaths)
+            {
+                Dictionary<int, (int, int)> wireLocationsWithSteps = GetWireLocationsWithSteps(wirePath.Value);
+                wiresLocations.Add(wirePath.Key, wireLocationsWithSteps.Values.ToList());
+            }
+
+            List<(int, int)> wiresIntersections =
+                wiresLocations[FIRST_WIRE].Intersect(wiresLocations[SECOND_WIRE]).ToList();
+
+            return wiresIntersections;
+        }
+
+        private Dictionary<int, (int, int)> GetWireLocationsWithSteps(List<Instruction> instructions)
+        {
+            Dictionary<int, (int, int)> wireLocations = new Dictionary<int, (int, int)>();
+
+            int totalSteps = 0;
             int x = 0;
             int y = 0;
 
@@ -41,40 +68,28 @@ namespace App.Tasks.Year2019.Day3
             {
                 int steps = instruction.Steps;
 
-                switch (instruction.TurnDirection)
+                while (steps > 0)
                 {
-                    case TurnDirection.Left:
-                        while (steps > 0)
-                        {
+                    switch (instruction.TurnDirection)
+                    {
+                        case TurnDirection.Left:
                             x--;
-                            wireLocations.Add((x, y));
-                            steps--;
-                        }
-                        break;
-                    case TurnDirection.Right:
-                        while (steps > 0)
-                        {
+                            break;
+                        case TurnDirection.Right:
                             x++;
-                            wireLocations.Add((x, y));
-                            steps--;
-                        }
-                        break;
-                    case TurnDirection.Up:
-                        while (steps > 0)
-                        {
+                            break;
+                        case TurnDirection.Up:
                             y++;
-                            wireLocations.Add((x, y));
-                            steps--;
-                        }
-                        break;
-                    case TurnDirection.Down:
-                        while (steps > 0)
-                        {
+                            break;
+                        case TurnDirection.Down:
                             y--;
-                            wireLocations.Add((x, y));
-                            steps--;
-                        }
-                        break;
+                            break;
+                    }
+
+                    steps--;
+                    totalSteps++;
+
+                    wireLocations.Add(totalSteps, (x, y));
                 }
             }
 
@@ -92,6 +107,28 @@ namespace App.Tasks.Year2019.Day3
             }
 
             return minManhattanDistance;
+        }
+
+        private int CalculateFewestCombinedSteps(
+            Dictionary<int, Dictionary<int, (int x, int y)>> wiresLocationsWithSteps,
+            List<(int, int)> wiresIntersections
+        )
+        {
+            int fewestCombinedSteps = int.MaxValue;
+
+            foreach ((int x, int y) in wiresIntersections)
+            {
+                int firstWireSteps = wiresLocationsWithSteps[FIRST_WIRE]
+                    .Where(wl => wl.Value.x == x && wl.Value.y == y).First().Key;
+                int secondWireSteps = wiresLocationsWithSteps[SECOND_WIRE]
+                    .Where(wl => wl.Value.x == x && wl.Value.y == y).First().Key;
+
+                int steps = firstWireSteps + secondWireSteps;
+
+                fewestCombinedSteps = Math.Min(steps, fewestCombinedSteps);
+            }
+
+            return fewestCombinedSteps;
         }
     }
 }
