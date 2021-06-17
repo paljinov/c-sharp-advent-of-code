1ac9b6eeecf83c3f301d5fbb71ec11b939a42ab7
Pave Aljinović <paljinov@users.noreply.github.com>
2021-06-17 09:52:08 +0200

    Add "Day 7: Amplification Circuit" part2 tests

---
+++ tests/Tasks/2019/Day07/Part2Tests.cs
@@ -0,0 +1,29 @@
+using App.Tasks.Year2019.Day7;
+using Xunit;
+
+namespace Tests.Tasks.Year2019.Day7
+{
+    public class Part2Tests
+    {
+        private readonly Part2 task;
+
+        public Part2Tests()
+        {
+            task = new Part2();
+        }
+
+        [Theory]
+        [InlineData("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,"
+            + "27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5", 139629729)]
+        [InlineData("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,"
+            + "-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,"
+            + "53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10", 18216)]
+        public void Solution_IntegersExample_HighestSignalThatCanBeSentToTheThrustersEquals(
+            string integers,
+            int highestSignalThatCanBeSentToTheThrusters
+        )
+        {
+            Assert.Equal(highestSignalThatCanBeSentToTheThrusters, task.Solution(integers));
+        }
+    }
+}
