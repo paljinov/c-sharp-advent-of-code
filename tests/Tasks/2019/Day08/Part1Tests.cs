e73fda836ae06b9265a5bb162f7d7c1f56d3c173
Pave Aljinović <paljinov@users.noreply.github.com>
2021-06-20 09:17:16 +0200

    Add "Day 8: Space Image Format" part1 tests

--- tests/Tasks/2019/Day08/Part1Tests.cs
+++ tests/Tasks/2019/Day08/Part1Tests.cs
@@ -6,5 +6,29 @@ namespace Tests.Tasks.Year2019.Day8
 {
     public class Part1Tests
     {
+        private readonly Part1 task;
+
+        public Part1Tests()
+        {
+            task = new Part1();
+
+            PasswordImage passwordImage = new PasswordImage();
+            typeof(PasswordImage)
+                .GetField("imageWidth", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(passwordImage, 3);
+            typeof(PasswordImage)
+                .GetField("imageHeight", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(passwordImage, 2);
+            typeof(Part1)
+                .GetField("passwordImage", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(task, passwordImage);
+        }
+
+        [Fact]
+        public void Solution_PasswordExample_NumberOneAndNumberTwoDigitsProductOfLayerThatContainsFewestZeroDigitsEquals()
+        {
+            string password = "123456789012";
+            Assert.Equal(1, task.Solution(password));
+        }
     }
 }
