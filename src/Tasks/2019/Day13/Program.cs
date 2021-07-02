2dc236ffa9bf62765e95115b79026046498a946a
Pave Aljinović <paljinovic@gmail.com>
2021-07-02 19:39:46 +0200

    Add "Day 13: Care Package" part1 solution

---
+++ src/Tasks/2019/Day13/Program.cs
@@ -0,0 +1,262 @@
+using System.Collections.Generic;
+
+namespace App.Tasks.Year2019.Day13
+{
+    public class Program
+    {
+        private const int BLOCK_TILE = 2;
+
+        public int CountBlockTiles(long[] integersArray)
+        {
+            List<Tile> tiles = GetTiles(integersArray);
+
+            int blockTiles = 0;
+            foreach (Tile tile in tiles)
+            {
+                if (tile.Id == BLOCK_TILE)
+                {
+                    blockTiles++;
+                }
+            }
+
+            return blockTiles;
+        }
+
+        private List<Tile> GetTiles(long[] integersArray)
+        {
+            List<Tile> tiles = new List<Tile>();
+
+            Dictionary<long, long> integers = InitIntegersMemory(integersArray);
+
+            long output;
+            bool halted = false;
+
+            // IntCode program current index and relative base value
+            long index = 0;
+            long relativeBase = 0;
+
+            while (!halted)
+            {
+                Tile tile = new Tile();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    (output, halted) = CalculateOutputSignal(integers, 0, ref index, ref relativeBase);
+
+                    if (!halted)
+                    {
+                        if (i == 0)
+                        {
+                            tile.X = output;
+                        }
+                        else if (i == 1)
+                        {
+                            tile.Y = output;
+                        }
+                        else
+                        {
+                            tile.Id = output;
+                        }
+                    }
+                }
+
+                if (!halted)
+                {
+                    tiles.Add(tile);
+                }
+            }
+
+            return tiles;
+        }
+
+        private (long, bool) CalculateOutputSignal(
+            Dictionary<long, long> integers,
+            int input,
+            ref long i,
+            ref long relativeBase
+        )
+        {
+            long? output = null;
+
+            while (integers[i] != (int)Operation.Halt)
+            {
+                if (output.HasValue)
+                {
+                    return (output.Value, false);
+                }
+
+                // Pad first instruction with leading zeros
+                string instruction = integers[i].ToString("D5");
+
+                int operation = (int)char.GetNumericValue(instruction[^1]);
+                int firstParameterModeDigit = (int)char.GetNumericValue(instruction[2]);
+                int secondParameterModeDigit = (int)char.GetNumericValue(instruction[1]);
+                int thirdParameterModeDigit = (int)char.GetNumericValue(instruction[0]);
+
+                ParameterMode firstParameterMode;
+                ParameterMode secondParameterMode;
+                ParameterMode thirdParameterMode;
+                long firstParameter;
+                long secondParameter;
+                long thirdParameter;
+
+                switch (operation)
+                {
+                    case (int)Operation.Addition:
+                    case (int)Operation.Multiplication:
+                    case (int)Operation.LessThan:
+                    case (int)Operation.Equals:
+                        firstParameterMode = GetParameterMode(firstParameterModeDigit);
+                        secondParameterMode = GetParameterMode(secondParameterModeDigit);
+                        thirdParameterMode = GetParameterMode(thirdParameterModeDigit);
+                        firstParameter = GetParameter(integers, i + 1, firstParameterMode, relativeBase);
+                        secondParameter = GetParameter(integers, i + 2, secondParameterMode, relativeBase);
+                        thirdParameter = GetParameter(integers, i + 3, thirdParameterMode, relativeBase, true);
+                        i += 4;
+
+                        if (operation == (int)Operation.Addition)
+                        {
+                            integers[thirdParameter] = firstParameter + secondParameter;
+                        }
+                        else if (operation == (int)Operation.Multiplication)
+                        {
+                            integers[thirdParameter] = firstParameter * secondParameter;
+                        }
+                        else if (operation == (int)Operation.LessThan)
+                        {
+                            integers[thirdParameter] = 0;
+                            if (firstParameter < secondParameter)
+                            {
+                                integers[thirdParameter] = 1;
+                            }
+                        }
+                        else if (operation == (int)Operation.Equals)
+                        {
+                            integers[thirdParameter] = 0;
+                            if (firstParameter == secondParameter)
+                            {
+                                integers[thirdParameter] = 1;
+                            }
+                        }
+                        break;
+                    case (int)Operation.Input:
+                    case (int)Operation.Output:
+                    case (int)Operation.RelativeBaseOffset:
+                        firstParameterMode = GetParameterMode(firstParameterModeDigit);
+                        firstParameter = GetParameter(
+                            integers, i + 1, firstParameterMode, relativeBase, operation == (int)Operation.Input);
+                        i += 2;
+
+                        if (operation == (int)Operation.Input)
+                        {
+                            integers[firstParameter] = input;
+                        }
+                        else if (operation == (int)Operation.Output)
+                        {
+                            output = (int)firstParameter;
+                        }
+                        else if (operation == (int)Operation.RelativeBaseOffset)
+                        {
+                            relativeBase += firstParameter;
+                        }
+                        break;
+                    case (int)Operation.JumpIfTrue:
+                    case (int)Operation.JumpIfFalse:
+                        firstParameterMode = GetParameterMode(firstParameterModeDigit);
+                        secondParameterMode = GetParameterMode(secondParameterModeDigit);
+                        firstParameter = GetParameter(integers, i + 1, firstParameterMode, relativeBase);
+                        secondParameter = GetParameter(integers, i + 2, secondParameterMode, relativeBase);
+                        i += 3;
+
+                        if (operation == (int)Operation.JumpIfTrue)
+                        {
+                            if (firstParameter != 0)
+                            {
+                                i = secondParameter;
+                            }
+                        }
+                        else if (operation == (int)Operation.JumpIfFalse)
+                        {
+                            if (firstParameter == 0)
+                            {
+                                i = secondParameter;
+                            }
+                        }
+                        break;
+                }
+            }
+
+            return (0, true);
+        }
+
+        private Dictionary<long, long> InitIntegersMemory(long[] integersArray)
+        {
+            Dictionary<long, long> integers = new Dictionary<long, long>();
+            for (int i = 0; i < integersArray.Length; i++)
+            {
+                integers.Add(i, integersArray[i]);
+            }
+
+            return integers;
+        }
+
+        private ParameterMode GetParameterMode(int mode)
+        {
+            ParameterMode parameterMode;
+
+            switch (mode)
+            {
+                case 1:
+                    parameterMode = ParameterMode.ImmediateMode;
+                    break;
+                case 2:
+                    parameterMode = ParameterMode.RelativeMode;
+                    break;
+                default:
+                    parameterMode = ParameterMode.PositionMode;
+                    break;
+            }
+
+            return parameterMode;
+        }
+
+        private long GetParameter(
+            Dictionary<long, long> integers,
+            long i,
+            ParameterMode mode,
+            long relativeBase,
+            bool instructionWritesTo = false
+        )
+        {
+            long parameter;
+
+            if (instructionWritesTo)
+            {
+                if (mode == ParameterMode.RelativeMode)
+                {
+                    return relativeBase + integers[i];
+                }
+                else
+                {
+                    return integers[i];
+                }
+            }
+
+            switch (mode)
+            {
+                case ParameterMode.ImmediateMode:
+                    parameter = integers[i];
+                    break;
+                case ParameterMode.RelativeMode:
+                    parameter = integers.ContainsKey(relativeBase + integers[i])
+                        ? integers[relativeBase + integers[i]] : 0;
+                    break;
+                default:
+                    parameter = integers.ContainsKey(integers[i]) ? integers[integers[i]] : 0;
+                    break;
+            }
+
+            return parameter;
+        }
+    }
+}
---
+++ src/Tasks/2019/Day13/Tile.cs
@@ -0,0 +1,9 @@
+namespace App.Tasks.Year2019.Day13
+{
+    public class Tile
+    {
+        public long X { get; set; }
+        public long Y { get; set; }
+        public long Id { get; set; }
+    }
+}
