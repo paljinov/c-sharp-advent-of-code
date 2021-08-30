ff9a06288833a414263b7433747d083b1f9d0dd0
Pave Aljinović <paljinovic@gmail.com>
2021-08-30 07:34:37 +0200

    Add "Day 16: Flawed Frequency Transmission" part2 solution

--- src/Tasks/2019/Day16/FlawedFrequencyTransmissionAlgorithm.cs
+++ src/Tasks/2019/Day16/FlawedFrequencyTransmissionAlgorithm.cs
@@ -14,6 +14,43 @@ namespace App.Tasks.Year2019.Day16
 
         public string CalculateFirstEightDigitsInTheFinalOutputList(int[] inputSignal)
         {
+            int[] outputList = CalculateFinalOutputList(inputSignal);
+
+            string firstEightDigitsInTheFinalOutputList = string.Empty;
+            for (int i = 0; i < EIGHT_DIGITS; i++)
+            {
+                firstEightDigitsInTheFinalOutputList += outputList[i];
+            }
+
+            return firstEightDigitsInTheFinalOutputList;
+        }
+
+        public string CalculateEightDigitMessageEmbeddedInTheFinalOutputList(
+            int[] inputSignal,
+            int inputSignalRepetitions,
+            int messageOffset
+        )
+        {
+            List<int> realInputSignal = new List<int>();
+
+            for (int i = 0; i < inputSignalRepetitions; i++)
+            {
+                realInputSignal = realInputSignal.Concat(inputSignal.ToList()).ToList();
+            }
+
+            int[] outputList = CalculateFinalOutputList(realInputSignal.ToArray());
+
+            string eightDigitMessageEmbeddedInTheFinalOutputList = string.Empty;
+            for (int i = messageOffset; i < messageOffset + EIGHT_DIGITS; i++)
+            {
+                eightDigitMessageEmbeddedInTheFinalOutputList += outputList[i];
+            }
+
+            return eightDigitMessageEmbeddedInTheFinalOutputList;
+        }
+
+        private int[] CalculateFinalOutputList(int[] inputSignal)
+        {
             int[] outputList = new int[inputSignal.Length];
 
             int[][] patternForEachPosition = GetPatternForEachPosition(inputSignal.Length);
@@ -30,13 +67,7 @@ namespace App.Tasks.Year2019.Day16
                 inputSignal = outputList;
             }
 
-            string firstEightDigitsInTheFinalOutputList = string.Empty;
-            for (int i = 0; i < EIGHT_DIGITS; i++)
-            {
-                firstEightDigitsInTheFinalOutputList += outputList[i];
-            }
-
-            return firstEightDigitsInTheFinalOutputList;
+            return outputList;
         }
 
         private int[][] GetPatternForEachPosition(int inputSignalLength)
