7209efbe7e9f1be3990b6b665fb07649ea661ded
Pave Aljinović <paljinovic@gmail.com>
2021-11-27 19:58:48 +0100

    Add "Day 23: Experimental Emergency Teleportation" part1 solution

--- src/Tasks/2018/Day23/ExperimentalEmergencyTeleportation.cs
+++ src/Tasks/2018/Day23/ExperimentalEmergencyTeleportation.cs
@@ -1,10 +1,57 @@
+using System;
+using System.Linq;
+
 namespace App.Tasks.Year2018.Day23
 {
     public class ExperimentalEmergencyTeleportation
     {
         public int CountNanobotsWhichAreInRangeOfTheLargestSignalRadius(Nanobot[] nanobots)
         {
-            return nanobots.Length;
+            Nanobot nanobotWithLargestSignalRadius = GetNanobotWithLargestSignalRadius(nanobots);
+
+            int nanobotsWhichAreInRangeOfTheLargestSignalRadius =
+                CountNanobotsWhichAreInRangeOfSignalRadius(nanobots, nanobotWithLargestSignalRadius);
+
+            return nanobotsWhichAreInRangeOfTheLargestSignalRadius;
+        }
+
+        private Nanobot GetNanobotWithLargestSignalRadius(Nanobot[] nanobots)
+        {
+            int largestSignalRadius = 0;
+            Nanobot nanobotWithLargestSignalRadius = nanobots.First();
+
+            foreach (Nanobot nanobot in nanobots)
+            {
+                if (nanobot.SignalRadius > largestSignalRadius)
+                {
+                    largestSignalRadius = nanobot.SignalRadius;
+                    nanobotWithLargestSignalRadius = nanobot;
+                }
+            }
+
+            return nanobotWithLargestSignalRadius;
+        }
+
+        private int CountNanobotsWhichAreInRangeOfSignalRadius(
+            Nanobot[] nanobots,
+            Nanobot nanobotWithLargestSignalRadius
+        )
+        {
+            int nanobotsWhichAreInRangeOfTheLargestSignalRadius = 0;
+
+            foreach (Nanobot nanobot in nanobots)
+            {
+                int manhattanDistance = Math.Abs(nanobotWithLargestSignalRadius.Position.X - nanobot.Position.X)
+                    + Math.Abs(nanobotWithLargestSignalRadius.Position.Y - nanobot.Position.Y)
+                    + Math.Abs(nanobotWithLargestSignalRadius.Position.Z - nanobot.Position.Z);
+
+                if (nanobotWithLargestSignalRadius.SignalRadius >= manhattanDistance)
+                {
+                    nanobotsWhichAreInRangeOfTheLargestSignalRadius++;
+                }
+            }
+
+            return nanobotsWhichAreInRangeOfTheLargestSignalRadius;
         }
     }
 }
