b40d0874b50eb10df0fb13118b4508b3d94cd20d
Pave Aljinović <paljinovic@gmail.com>
2021-12-02 08:55:29 +0100

    Add "Day 2: Dive!" part2 solution

--- src/Tasks/2021/Day02/Dive.cs
+++ src/Tasks/2021/Day02/Dive.cs
@@ -27,5 +27,33 @@ namespace App.Tasks.Year2021.Day2
 
             return productOfFinalHorizontalPositionAndDept;
         }
+
+        public int CalculateProductOfFinalHorizontalPositionAndDeptWhenTrackingAim(Instruction[] instructions)
+        {
+            int horizontalPosition = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (Instruction instruction in instructions)
+            {
+                if (instruction.Command == Command.Forward)
+                {
+                    horizontalPosition += instruction.Value;
+                    depth += aim * instruction.Value;
+                }
+                else if (instruction.Command == Command.Down)
+                {
+                    aim += instruction.Value;
+                }
+                else
+                {
+                    aim -= instruction.Value;
+                }
+            }
+
+            int productOfFinalHorizontalPositionAndDeptWhenTrackingAim = horizontalPosition * depth;
+
+            return productOfFinalHorizontalPositionAndDeptWhenTrackingAim;
+        }
     }
 }
