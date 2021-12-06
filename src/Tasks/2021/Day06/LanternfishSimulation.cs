7e74945273b9b5470dbacca18120ff45668570a9
Pave Aljinović <paljinovic@gmail.com>
2021-12-06 18:50:00 +0100

    Add "Day 6: Lanternfish" part1 solution

--- src/Tasks/2021/Day06/LanternfishSimulation.cs
+++ src/Tasks/2021/Day06/LanternfishSimulation.cs
@@ -1,10 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace App.Tasks.Year2021.Day6
 {
     public class LanternfishSimulation
     {
+        private const int CREATOR_LANTERNFISH_INTERNAL_TIMER = 6;
+
+        private const int NEW_LANTERNFISH_INTERNAL_TIMER = 8;
+
         public int CountLanternfishAfterGivenDays(int[] lanternfishInternalTimers, int totalDays)
         {
-            return lanternfishInternalTimers.Length;
+            List<int> lanternfish = lanternfishInternalTimers.ToList();
+
+            for (int day = 1; day <= totalDays; day++)
+            {
+                int lanternfishCount = lanternfish.Count;
+                for (int i = 0; i < lanternfishCount; i++)
+                {
+                    lanternfish[i]--;
+                    // Create new lanternfish
+                    if (lanternfish[i] < 0)
+                    {
+                        lanternfish[i] = CREATOR_LANTERNFISH_INTERNAL_TIMER;
+                        lanternfish.Add(NEW_LANTERNFISH_INTERNAL_TIMER);
+                    }
+                }
+            }
+
+            return lanternfish.Count;
         }
     }
 }
