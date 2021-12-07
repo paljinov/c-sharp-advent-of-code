8b6c454fefc9e3b5b8cd711d459fa581accc19fe
Pave Aljinović <paljinovic@gmail.com>
2021-12-07 08:44:45 +0100

    Add "Day 7: The Treachery of Whales" part1 solution

--- src/Tasks/2021/Day07/CrabSubmarines.cs
+++ src/Tasks/2021/Day07/CrabSubmarines.cs
@@ -1,10 +1,29 @@
+using System;
+using System.Linq;
+
 namespace App.Tasks.Year2021.Day7
 {
     public class CrabSubmarines
     {
         public int CalculateLeastSpentFuelWhichIsNeededToAlign(int[] crabHorizontalPositions)
         {
-            return crabHorizontalPositions.Length;
+            int leastSpentFuel = int.MaxValue;
+
+            int minHorizontalPosition = crabHorizontalPositions.Min();
+            int maxHorizontalPosition = crabHorizontalPositions.Max();
+
+            for (int hp = minHorizontalPosition; hp < maxHorizontalPosition; hp++)
+            {
+                int spentFuel = 0;
+                foreach (int crabHorizontalPosition in crabHorizontalPositions)
+                {
+                    spentFuel += Math.Abs(crabHorizontalPosition - hp);
+                }
+
+                leastSpentFuel = Math.Min(leastSpentFuel, spentFuel);
+            }
+
+            return leastSpentFuel;
         }
     }
 }
