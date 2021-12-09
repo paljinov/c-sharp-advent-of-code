2fc7ad49de8d0e9301ea9dcf74a582de8ab94d2e
Pave Aljinović <paljinovic@gmail.com>
2021-12-09 08:20:42 +0100

    Add "Day 9: Smoke Basin" skeleton

---
+++ src/Tasks/2021/Day09/Heightmap.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+namespace App.Tasks.Year2021.Day9
+{
+    public class Heightmap
+    {
+        public int CalculateRiskLevelsSumOfAllHeightmapLowPoints(int[,] heightmap)
+        {
+            return 0;
+        }
+    }
+}
---
+++ src/Tasks/2021/Day09/HeightmapRepository.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace App.Tasks.Year2021.Day9
+{
+    public class HeightmapRepository
+    {
+        public int[,] GetHeightmap(string input)
+        {
+            string[] heightmapString = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            int rows = heightmapString.Length;
+            int columns = heightmapString[0].Length;
+            int[,] heightmap = new int[rows, columns];
+
+            for (int i = 0; i < heightmapString.Length; i++)
+            {
+                for (int j = 0; j < heightmapString[i].Length; j++)
+                {
+
+                    heightmap[i, j] = (int)char.GetNumericValue(heightmapString[i][j]);
+                }
+            }
+
+            return heightmap;
+        }
+    }
+}
