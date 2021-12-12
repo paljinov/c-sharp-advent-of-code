511db2db59f12d2302ab583e3e0c5ba26bee28b4
Pave Aljinović <paljinovic@gmail.com>
2021-12-12 12:31:52 +0100

    Add "Day 12: Passage Pathing" part2 tests

---
+++ tests/Tasks/2021/Day12/Part2Tests.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using App.Tasks.Year2021.Day12;
+using Xunit;
+
+namespace Tests.Tasks.Year2021.Day12
+{
+    public class Part2Tests
+    {
+        private readonly Part2 task;
+
+        public Part2Tests()
+        {
+            task = new Part2();
+        }
+
+        [Theory]
+        [ClassData(typeof(CaveSystem_CaveSystemPathsThatVisitSmallCavesAtMostTwiceCount_TestData))]
+        public void Solution_CaveSystemExample_CaveSystemPathsThatVisitSmallCavesAtMostTwiceCountEquals(
+            string caveSystem,
+            int caveSystemPathsThatVisitSmallCavesAtMostTwice
+        )
+        {
+            Assert.Equal(caveSystemPathsThatVisitSmallCavesAtMostTwice, task.Solution(caveSystem));
+        }
+
+        public class CaveSystem_CaveSystemPathsThatVisitSmallCavesAtMostTwiceCount_TestData
+            : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] {
+                    "start-A"
+                    + $"{Environment.NewLine}start-b"
+                    + $"{Environment.NewLine}A-c"
+                    + $"{Environment.NewLine}A-b"
+                    + $"{Environment.NewLine}b-d"
+                    + $"{Environment.NewLine}A-end"
+                    + $"{Environment.NewLine}b-end",
+                    36
+                };
+
+                yield return new object[] {
+                    "dc-end"
+                    + $"{Environment.NewLine}HN-start"
+                    + $"{Environment.NewLine}start-kj"
+                    + $"{Environment.NewLine}dc-start"
+                    + $"{Environment.NewLine}dc-HN"
+                    + $"{Environment.NewLine}LN-dc"
+                    + $"{Environment.NewLine}HN-end"
+                    + $"{Environment.NewLine}kj-sa"
+                    + $"{Environment.NewLine}kj-HN"
+                    + $"{Environment.NewLine}kj-dc",
+                    103
+                };
+
+                yield return new object[] {
+                    "fs-end"
+                    + $"{Environment.NewLine}he-DX"
+                    + $"{Environment.NewLine}fs-he"
+                    + $"{Environment.NewLine}start-DX"
+                    + $"{Environment.NewLine}pj-DX"
+                    + $"{Environment.NewLine}end-zg"
+                    + $"{Environment.NewLine}zg-sl"
+                    + $"{Environment.NewLine}zg-pj"
+                    + $"{Environment.NewLine}pj-he"
+                    + $"{Environment.NewLine}RW-he"
+                    + $"{Environment.NewLine}fs-DX"
+                    + $"{Environment.NewLine}pj-RW"
+                    + $"{Environment.NewLine}zg-RW"
+                    + $"{Environment.NewLine}start-pj"
+                    + $"{Environment.NewLine}he-WI"
+                    + $"{Environment.NewLine}zg-he"
+                    + $"{Environment.NewLine}pj-fs"
+                    + $"{Environment.NewLine}start-RW",
+                    3509
+                };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
