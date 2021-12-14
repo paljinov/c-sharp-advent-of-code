f5021439f65edde0a8257f203adbe1a4c7cc5a57
Pave Aljinović <paljinovic@gmail.com>
2021-12-14 08:48:36 +0100

    Add "Day 14: Extended Polymerization" part1 solution

--- src/Tasks/2021/Day14/Polymer.cs
+++ src/Tasks/2021/Day14/Polymer.cs
@@ -12,7 +12,38 @@ namespace App.Tasks.Year2021.Day14
             int totalSteps
         )
         {
-            return 0;
+            string polymer = polymerTemplate.ToString();
+
+            for (int step = 1; step <= totalSteps; step++)
+            {
+                polymer = DoStep(polymer, pairInsertionRules);
+            }
+
+            var letterOccurences = polymer.GroupBy(l => l).OrderByDescending(l => l.Count());
+
+            int differenceBetweenMostAndLeastCommonElement =
+                letterOccurences.First().Count() - letterOccurences.Last().Count();
+
+            return differenceBetweenMostAndLeastCommonElement;
+        }
+
+        private string DoStep(string polymer, Dictionary<string, char> pairInsertionRules)
+        {
+            StringBuilder newPolymer = new StringBuilder();
+
+            for (int i = 0; i < polymer.Length - 1; i++)
+            {
+                newPolymer.Append(polymer[i]);
+
+                string pair = new string(new char[] { polymer[i], polymer[i + 1] });
+                if (pairInsertionRules.ContainsKey(pair))
+                {
+                    newPolymer.Append(pairInsertionRules[pair]);
+                }
+            }
+            newPolymer.Append(polymer[^1]);
+
+            return newPolymer.ToString();
         }
     }
 }
