47aa732429a4731e7ebcbd8edc4f753546685455
Pave Aljinović <paljinovic@gmail.com>
2021-12-21 18:38:09 +0100

    Add "Day 21: Dirac Dice" skeleton

---
+++ src/Tasks/2021/Day21/DiracDice.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace App.Tasks.Year2021.Day21
+{
+    public class DiracDice
+    {
+        public int CalculateProductOfLosingPlayerScoreMultipliedByNumberOfDieRolls(
+            Dictionary<int, int> playersStartingPositions
+        )
+        {
+            return 0;
+        }
+    }
+}
---
+++ src/Tasks/2021/Day21/PlayersRepository.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace App.Tasks.Year2021.Day21
+{
+    public class NewBaseType
+    {
+        public Dictionary<int, int> GetPlayersStartingPositions(string input)
+        {
+            Dictionary<int, int> playersStartingPositions = new Dictionary<int, int>();
+            string[] playersStartingPositionsString =
+                input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            Regex playerStartingPositionRegex = new Regex(@"^Player\s(\d+)\sstarting\sposition:\s(\d+)$");
+
+            for (int i = 0; i < playersStartingPositionsString.Length; i++)
+            {
+                Match playerStartingPositionMatch =
+                    playerStartingPositionRegex.Match(playersStartingPositionsString[i]);
+                GroupCollection playerStartingPositionGroups = playerStartingPositionMatch.Groups;
+
+                int playerId = int.Parse(playerStartingPositionGroups[1].Value);
+                int playerStartingPosition = int.Parse(playerStartingPositionGroups[2].Value);
+
+                playersStartingPositions.Add(playerId, playerStartingPosition);
+            }
+
+            return playersStartingPositions;
+        }
+    }
+
+    public class PlayersRepository : NewBaseType
+    {
+    }
+}
