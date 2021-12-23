e0e49fe53cd7d12de5a52f778950b226ef958df0
Pave Aljinović <paljinovic@gmail.com>
2021-12-23 22:18:50 +0100

    Add "Day 22: Reactor Reboot" part1 solution

--- src/Tasks/2021/Day22/Cubes.cs
+++ src/Tasks/2021/Day22/Cubes.cs
@@ -1,10 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace App.Tasks.Year2021.Day22
 {
     public class Cubes
     {
         public int CalculateTurnedOnCubesInRegion(RebootStep[] rebootSteps, Cuboid region)
         {
-            return 0;
+            Dictionary<(int X, int Y, int Z), bool> regionCubes = new Dictionary<(int X, int Y, int Z), bool>();
+
+            foreach (RebootStep rebootStep in rebootSteps)
+            {
+                (int From, int To) xRange = (
+                    Math.Max(region.X.From, rebootStep.Cuboid.X.From),
+                    Math.Min(region.X.To, rebootStep.Cuboid.X.To)
+                );
+
+                (int From, int To) yRange = (
+                    Math.Max(region.Y.From, rebootStep.Cuboid.Y.From),
+                    Math.Min(region.Y.To, rebootStep.Cuboid.Y.To)
+                );
+
+                (int From, int To) zRange = (
+                    Math.Max(region.Z.From, rebootStep.Cuboid.Z.From),
+                    Math.Min(region.Z.To, rebootStep.Cuboid.Z.To)
+                );
+
+                for (int x = xRange.From; x <= xRange.To; x++)
+                {
+                    for (int y = yRange.From; y <= yRange.To; y++)
+                    {
+                        for (int z = zRange.From; z <= zRange.To; z++)
+                        {
+                            regionCubes[(x, y, z)] = false;
+                            if (rebootStep.Action == Action.TurnOn)
+                            {
+                                regionCubes[(x, y, z)] = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            int turnedOnCubesInRegion = regionCubes.Count(c => c.Value == true);
+
+            return turnedOnCubesInRegion;
         }
     }
 }
