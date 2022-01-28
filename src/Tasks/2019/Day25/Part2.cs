ed5b291726c71a0e0865df9ff5c1f417d69bc21b
Pave Aljinović <paljinovic@gmail.com>
2022-01-28 22:55:32 +0100

    Add "Day 25: Cryostasis" part2 solution

---
+++ src/Tasks/2019/Day25/Part2.cs
@@ -0,0 +1,45 @@
+/*
+--- Part Two ---
+
+As you move through the main airlock, the air inside the ship is already heating
+up to reasonable levels. Santa explains that he didn't notice you coming because
+he was just taking a quick nap. The ship wasn't frozen; he just had the
+thermostat set to "North Pole".
+
+You make your way over to the navigation console. It beeps. "Status: Stranded.
+Please supply measurements from 49 stars to recalibrate."
+
+"49 stars? But the Elves told me you needed fifty--"
+
+Santa just smiles and nods his head toward the window. There, in the distance,
+you can see the center of the Solar System: the Sun!
+
+The navigation console beeps again.
+*/
+
+/*
+################################################################################
+Part 2 is automatically solved if all of the puzzles before are solved, it was
+just needed to click on the link to get part2 star.
+################################################################################
+*/
+
+/*
+You use all fifty stars to align the warp drive! Suddenly, the Sun starts
+getting a lot closer.
+
+You'll be home before you know it.
+
+Congratulations! You've finished every puzzle in Advent of Code 2019! I hope you
+had as much fun solving them as I had making them for you. I'd love to hear
+about your adventure; you can get in touch with me via contact info on my
+website or through Twitter.
+
+If you'd like to see more things like this in the future, please consider
+supporting Advent of Code and sharing it with others.
+
+To hear about future projects, you can follow me on Twitter.
+
+I've highlighted the easter eggs in each puzzle, just in case you missed any.
+Hover your mouse over them, and the easter egg will appear.
+*/
