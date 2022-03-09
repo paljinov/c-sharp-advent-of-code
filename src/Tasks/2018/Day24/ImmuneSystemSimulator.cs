8cb87f69038d942e157fce61b2207edcf99b7cfe
Pave Aljinović <paljinovic@gmail.com>
2022-03-09 16:16:03 +0100

    Add "Day 24: Immune System Simulator 20XX" skeleton

---
+++ src/Tasks/2018/Day24/ImmuneSystemSimulator.cs
@@ -0,0 +1,11 @@
+namespace App.Tasks.Year2018.Day24
+{
+    public class ImmuneSystemSimulator
+    {
+
+        public int CountWinningArmyUnits(string reindeerCondition)
+        {
+            return reindeerCondition.Length;
+        }
+    }
+}
---
+++ src/Tasks/2018/Day24/ReindeerConditionRepository.cs
@@ -0,0 +1,10 @@
+namespace App.Tasks.Year2018.Day24
+{
+    public class ReindeerConditionRepository
+    {
+        public string GetReindeerCondition(string input)
+        {
+            return input;
+        }
+    }
+}
