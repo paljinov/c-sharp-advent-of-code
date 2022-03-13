5b1496936dd2839ff6185e723398500d7d0a6d87
Pave Aljinović <paljinovic@gmail.com>
2022-03-13 22:54:48 +0100

    Add "Day 24: Immune System Simulator 20XX" part 2 skeleton

---
+++ src/Tasks/2018/Day24/GroupType.cs
@@ -0,0 +1,8 @@
+namespace App.Tasks.Year2018.Day24
+{
+    public enum GroupType
+    {
+        ImmuneSystem,
+        Infection
+    }
+}
--- src/Tasks/2018/Day24/ImmuneSystemSimulator.cs
+++ src/Tasks/2018/Day24/ImmuneSystemSimulator.cs
@@ -1,10 +1,49 @@
+using System.Linq;
+
 namespace App.Tasks.Year2018.Day24
 {
     public class ImmuneSystemSimulator
     {
         public int CountWinningArmyUnits(Group[] immuneSystemArmy, Group[] infectionArmy)
         {
+            while (immuneSystemArmy.Length > 0 && infectionArmy.Length > 0)
+            {
+                SortAttackersByInitiative(immuneSystemArmy, infectionArmy);
+            }
+
             return immuneSystemArmy.Length + infectionArmy.Length;
+        }
+
+        public int CountImmuneSystemUnitsWhichAreLeftAfterGettingTheSmallestBoostNeededToWin(
+            Group[] immuneSystemArmy,
+            Group[] infectionArmy
+        )
+        {
+            while (immuneSystemArmy.Length > 0 && infectionArmy.Length > 0)
+            {
+                SortAttackersByInitiative(immuneSystemArmy, infectionArmy);
+            }
+
+            return immuneSystemArmy.Length + infectionArmy.Length;
+        }
+
+        private void SortAttackersByInitiative(Group[] immuneSystemArmy, Group[] infectionArmy)
+        {
+            immuneSystemArmy = immuneSystemArmy.OrderByDescending(isa => isa.Initiative).ToArray();
+            infectionArmy = infectionArmy.OrderByDescending(ia => ia.Initiative).ToArray();
+        }
+
+        private void SortDefendersByEffectivePower(Group[] immuneSystemArmy, Group[] infectionArmy)
+        {
+            immuneSystemArmy = immuneSystemArmy
+                .OrderBy(isa => isa.Units * isa.UnitAttackDamage)
+                .ThenByDescending(isa => isa.Initiative)
+                .ToArray();
+
+            infectionArmy = infectionArmy
+                .OrderBy(ia => ia.Units * ia.UnitAttackDamage)
+                .ThenByDescending(ia => ia.Initiative)
+                .ToArray();
         }
     }
 }
