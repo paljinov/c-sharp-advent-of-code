0e233d9be1a86792df6d01ea63eca8069e76366a
Pave Aljinović <paljinovic@gmail.com>
2022-04-05 21:37:01 +0200

    Add "Day 17: Reservoir Research" skeleton

---
+++ src/Tasks/2018/Day17/ScanRepository.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace App.Tasks.Year2018.Day17
+{
+    public class ScanRepository
+    {
+        public string GetScan(string input)
+        {
+            return input;
+        }
+    }
+}
---
+++ src/Tasks/2018/Day17/Tiles.cs
@@ -0,0 +1,10 @@
+namespace App.Tasks.Year2018.Day17
+{
+    public class Tiles
+    {
+        public int CountTilesTheWaterCanReach(string scan)
+        {
+            return scan.Length;
+        }
+    }
+}
