0396888004ee001dd6e31a461f48efacbb0758eb
Pave Aljinović <paljinovic@gmail.com>
2022-04-25 21:13:19 +0200

    Initialize map for "Day 17: Reservoir Research"

---
+++ src/Tasks/2018/Day17/TileType.cs
@@ -0,0 +1,11 @@
+namespace App.Tasks.Year2018.Day17
+{
+    public enum TileType
+    {
+        Clay = '#',
+        Sand = '.',
+        SandWaterPassed = '|',
+        WaterSpring = '+',
+        RestWater = '~'
+    }
+}
--- src/Tasks/2018/Day17/Tiles.cs
+++ src/Tasks/2018/Day17/Tiles.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace App.Tasks.Year2018.Day17
 {
     public class Tiles
@@ -6,12 +9,70 @@ namespace App.Tasks.Year2018.Day17
 
         public int CountTilesTheWaterCanReach(ClayVein[] clayVeins)
         {
-            return clayVeins.Length;
+            Dictionary<(int, int), char> map = GetMap(clayVeins);
+
+            int tilesTheWaterCanReach = 0;
+
+            return tilesTheWaterCanReach;
         }
 
         public int CountLeftWaterTiles(ClayVein[] clayVeins)
         {
-            return clayVeins.Length;
+            Dictionary<(int, int), char> map = GetMap(clayVeins);
+
+            int leftWaterTiles = 0;
+
+            return leftWaterTiles;
+        }
+
+        private Dictionary<(int, int), char> GetMap(ClayVein[] clayVeins)
+        {
+            int minY = clayVeins.Select(cv => cv.XFrom).Min();
+            int maxY = clayVeins.Select(cv => cv.XTo).Max();
+            int minX = clayVeins.Select(cv => cv.YFrom).Min();
+            int maxX = clayVeins.Select(cv => cv.YTo).Max();
+
+            Dictionary<(int, int), char> map = InitializeScannedMap(clayVeins);
+
+            return map;
+        }
+
+        private Dictionary<(int, int), char> InitializeScannedMap(ClayVein[] clayVeins)
+        {
+            int minX = clayVeins.Select(cv => cv.XFrom).Min() - 1;
+            int maxX = clayVeins.Select(cv => cv.XTo).Max() + 1;
+            int minY = clayVeins.Select(cv => cv.YFrom).Min();
+            int maxY = clayVeins.Select(cv => cv.YTo).Max();
+
+            Dictionary<(int, int), char> scannedMap = new Dictionary<(int, int), char>();
+            for (int i = minX; i <= maxY; i++)
+            {
+                for (int j = minY; j <= maxY; j++)
+                {
+                    scannedMap[(i, j)] = (char)TileType.Sand;
+                }
+            }
+
+            scannedMap[(springOfWater.X, springOfWater.Y)] = (char)TileType.WaterSpring;
+            foreach (ClayVein clayVein in clayVeins)
+            {
+                if (clayVein.XFrom == clayVein.XTo)
+                {
+                    for (int i = clayVein.YFrom; i <= clayVein.YTo; i++)
+                    {
+                        scannedMap[(clayVein.XFrom, i)] = (char)TileType.Clay;
+                    }
+                }
+                else
+                {
+                    for (int i = clayVein.XFrom; i <= clayVein.XTo; i++)
+                    {
+                        scannedMap[(i, clayVein.YFrom)] = (char)TileType.Clay;
+                    }
+                }
+            }
+
+            return scannedMap;
         }
     }
 }
