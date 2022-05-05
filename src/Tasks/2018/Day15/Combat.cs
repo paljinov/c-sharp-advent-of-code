c813598f560cba150dca931cc552b138a10325cf
Pave Aljinović <paljinovic@gmail.com>
2022-05-05 21:50:08 +0200

    Initialize "Day 15: Beverage Bandits" units

--- src/Tasks/2018/Day15/Combat.cs
+++ src/Tasks/2018/Day15/Combat.cs
@@ -6,16 +6,16 @@ namespace App.Tasks.Year2018.Day15
 {
     public class Combat
     {
-        private const char GOBLIN = 'G';
-        private const char ELF = 'E';
         private const char OPEN_SPACE = '.';
+        private const char WALL = '#';
         private const int UNIT_ATTACK_POWER = 3;
         private const int UNIT_HIT_POINTS = 200;
-
 
         public int CalculateCombatOutcome(char[,] map)
         {
             int combatOutcome = 0;
+
+            List<Unit> units = InitializeUnits(map);
 
             return combatOutcome;
         }
@@ -25,6 +25,36 @@ namespace App.Tasks.Year2018.Day15
             int combatOutcomeOfTheBattleWithoutAnyElvesDying = 0;
 
             return combatOutcomeOfTheBattleWithoutAnyElvesDying;
+        }
+
+        public List<Unit> InitializeUnits(char[,] map)
+        {
+            List<Unit> units = new List<Unit>();
+
+            List<char> openSpaceOrWall = new List<char>() { OPEN_SPACE, WALL };
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    if (!openSpaceOrWall.Contains(map[x, y]))
+                    {
+                        UnitType unitType = (UnitType)map[x, y];
+                        int attackPower = UNIT_ATTACK_POWER;
+
+                        Unit unit = new Unit
+                        {
+                            Position = (x, y),
+                            UnitType = unitType,
+                            HitPoints = UNIT_HIT_POINTS,
+                            AttackPower = attackPower
+                        };
+
+                        units.Add(unit);
+                    }
+                }
+            }
+
+            return units;
         }
     }
 }
