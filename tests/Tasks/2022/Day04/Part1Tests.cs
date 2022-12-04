3430f2040223377d083800f9bb28e01081d9940d
Pave Aljinović <paljinovic@gmail.com>
2022-12-04 09:53:52 +0100

    Add "Day 4: Camp Cleanup" tests

---
+++ tests/Tasks/2022/Day04/Part1Tests.cs
@@ -0,0 +1,29 @@
+using System;
+using App.Tasks.Year2022.Day4;
+using Xunit;
+
+namespace Tests.Tasks.Year2022.Day4
+{
+    public class Part1Tests
+    {
+        private readonly Part1 task;
+
+        public Part1Tests()
+        {
+            task = new Part1();
+        }
+
+        [Fact]
+        public void Solution_PairsSectionsExample_PairsWhereOneRangeFullyContainTheOtherCountEquals()
+        {
+            string pairsSections = "2-4,6-8"
+                + $"{Environment.NewLine}2-3,4-5"
+                + $"{Environment.NewLine}5-7,7-9"
+                + $"{Environment.NewLine}2-8,3-7"
+                + $"{Environment.NewLine}6-6,4-6"
+                + $"{Environment.NewLine}2-6,4-8";
+
+            Assert.Equal(2, task.Solution(pairsSections));
+        }
+    }
+}
---
+++ tests/Tasks/2022/Day04/Part2Tests.cs
@@ -0,0 +1,29 @@
+using System;
+using App.Tasks.Year2022.Day4;
+using Xunit;
+
+namespace Tests.Tasks.Year2022.Day4
+{
+    public class Part2Tests
+    {
+        private readonly Part2 task;
+
+        public Part2Tests()
+        {
+            task = new Part2();
+        }
+
+        [Fact]
+        public void Solution_PairsSectionsExample_PairsWhereRangesOverlapCountEquals()
+        {
+            string pairsSections = "2-4,6-8"
+                + $"{Environment.NewLine}2-3,4-5"
+                + $"{Environment.NewLine}5-7,7-9"
+                + $"{Environment.NewLine}2-8,3-7"
+                + $"{Environment.NewLine}6-6,4-6"
+                + $"{Environment.NewLine}2-6,4-8";
+
+            Assert.Equal(4, task.Solution(pairsSections));
+        }
+    }
+}
